8b7db6d8a5b4cdd16fd41c9df80457d59bae61c6
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-06-16 01:06:44 -0400

    Adicionando a função decimal_para()

--- numero.asm
+++ numero.asm
@@ -1,6 +1,7 @@
 .data
 .align 0
 	digitos: .asciiz "0123456789ABCDEF"
+	convertido: .space 33
 
 .text
 
@@ -51,6 +52,7 @@ retorno_valida:
 	
 	jr $ra
 
+
 # Converte um número para decimal
 # Argumentos:
 #  $a0 - número
@@ -100,6 +102,54 @@ retorno_para_decimal:
 	
 	jr $ra
 
+
+# Converte um decimal para base
+# Argumentos:
+#  $a0 - decimal
+#  $a1 - base
+# Retorno:
+#  $v0 - convertido
+.globl decimal_para
+decimal_para:
+	# empilha
+	subi $sp, $sp, 4
+	sw $ra, 0($sp)
+	
+	beqz $a0, zero_decimal_para		# se decimal = 0 então retorna "0"
+	li $t0, 0				# tamanho <- 0
+						#
+enquanto_decimal_para:				# enquanto decimal != 0 faça
+	beqz $a0, inverte_decimal_para		#
+	divu $a0, $a1				#
+	mflo $a0				# 	decimal <- decimal / base
+	mfhi $t1				# 	índice <- decimal % base
+	lb $t1, digitos($t1)			# 	dígito <- dígitos[índice]
+	sb $t1, convertido($t0)			# 	convertido[tamanho] <- dígito
+	addi $t0, $t0, 1			# 	tamanho++
+	j enquanto_decimal_para			# fim enquanto
+						#
+inverte_decimal_para:				#
+	li $t1, '\0'				#
+	sb $t1, convertido($t0)			# convertido[tamanho] <- '\0'
+	la $a0, convertido			#
+	move $a1, $t0				#
+	jal inverte				# inverte(convertido, tamanho)
+	j retorno_decimal_para			# retorna convertido
+
+zero_decimal_para:
+	li $t1, '0'
+	sb $t1, convertido
+	li $t1, '\0'
+	sb $t1, convertido+1
+
+retorno_decimal_para:
+	la $v0, convertido
+
+	# desempilha
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
+	jr $ra
 
 .globl binario_para_decimal
 binario_para_decimal:
