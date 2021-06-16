fe70c696aefe2aacc09166e624065152680aee09
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2021-06-15 23:59:46 -0400

    Adicionando a função para_decimal()

--- numero.asm
+++ numero.asm
@@ -51,6 +51,55 @@ retorno_valida:
 	
 	jr $ra
 
+# Converte um número para decimal
+# Argumentos:
+#  $a0 - número
+#  $a1 - base
+# Retorno:
+#  $v0 - decimal
+# Registradores:
+#  $s0 - número
+#  $s1 - base
+#  $s2 - decimal
+.globl para_decimal
+para_decimal:
+	# empilha
+	subi $sp, $sp, 16
+	sw $s0, 12($sp)
+	sw $s1, 8($sp)
+	sw $s2, 4($sp)
+	sw $ra, 0($sp)
+	
+	# salva número e base
+	move $s0, $a0
+	move $s1, $a1
+	
+	li $s2, 0				# decimal <- 0
+						#
+enquanto_para_decimal:				# enquanto *número != '\0' e *número != '\n' faça
+	lb $a0, ($s0)				# 	dígito <- *número
+	beq $a0, '\0', retorno_para_decimal	#
+	beq $a0, '\n', retorno_para_decimal	#
+	mulu $s2, $s2, $s1			# 	decimal *= base
+	la $a1, digitos				#
+	move $a2, $s1				#
+	jal busca				# 	valor <- busca(dígito, dígitos, base)
+	addu $s2, $s2, $v0			# 	decimal += valor
+	addi $s0, $s0, 1			#	próximo(número)
+	j enquanto_para_decimal			# fim enquanto
+
+retorno_para_decimal:
+	move $v0, $s2
+
+	# desempilha
+	lw $s0, 12($sp)
+	lw $s1, 8($sp)
+	lw $s2, 4($sp)
+	lw $ra, 0($sp)
+	addi $sp, $sp, 16
+	
+	jr $ra
+
 
 .globl binario_para_decimal
 binario_para_decimal:
